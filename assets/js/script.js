255a06f30a9fd218349ba462b11e53ebed3e4199
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2023-04-27 13:24:30 -0500

    finish-page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
-const activeLink = document.querySelector(".active-link");
-const navLinks = document.querySelectorAll(".nav-link");
+const toggleSwitch = document.querySelector('.toggle-switch input[type="checkbox"]');
+const body = document.querySelector('body');
+const currentTheme = localStorage.getItem('theme');
 
-navLinks.forEach((link) => {
-   link.addEventListener("click", (e) => {
-      activeLink.classList.remove("active-link");
-      link.classList.add("active-link");
-   });
+if (currentTheme) {
+   body.classList.add(currentTheme);
+   if (currentTheme === 'dark-mode') {
+      toggleSwitch.checked = true;
    }
-);
+}
+
+toggleSwitch.addEventListener('change', function () {
+   if (this.checked) {
+      body.classList.add('dark-mode');
+      localStorage.setItem('theme', 'dark-mode');
+   } else {
+      body.classList.remove('dark-mode');
+      localStorage.setItem('theme', 'light-mode');
+   }
+});
